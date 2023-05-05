907c7f57c6ceca9a70f100b218cad08251ea3c70
ZRomik <zromik@mail.ru>
2023-05-05 12:40:52 +0400

    Реализована передача имени скрипта в функцию приветствия.

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello World from main.py!")
+import sys
+import os
+def say_hello(_from: str) -> None:
+    print(f"Hello World from {_from}!")
+
+if __name__  == "__main__":
+    say_hello(os.path.basename(__file__))
