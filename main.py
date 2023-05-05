333298c322a625155b48195a0a729fbacb3951ab
ZRomik <zromik@mail.ru>
2023-05-05 12:54:39 +0400

    функция приветсвия вынесена в отдельный файл

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 import os
+from helloprinter import print_hello
 def say_hello(_from: str) -> None:
     print(f"Hello World from {_from}!")
 
 if __name__  == "__main__":
-    say_hello(os.path.basename(__file__))
+    print_hello(os.path.basename(__file__))
